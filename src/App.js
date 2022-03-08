3007244071fec193a7d4850643ef0b31f11ed78e
Lakita Gaither <lakitagaither@yahoo.com>
2022-03-07 18:28:41 -0600

    Test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Advice Generator App
+        Advice Generator App(Practice)
       </header>
     </div>
   );
