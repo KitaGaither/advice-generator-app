b0237a9a2251c0f4ed8dfcd98f10f2141b933b62
Lakita Gaither <lakitagaither@yahoo.com>
2022-03-07 22:54:28 -0600

    Comments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+//These are my imports
 import React, { useState, useEffect } from "react";
 import './App.css';
 
+//This is my App
 const App = () => {
+  //This is my useState hook that sets the state
   const[quotes, setQuotes] = useState("");
 
+  //This is my getQuote function
   const getQuote = () => {
     fetch("https://type.fit/api/quotes")
     .then((res) => res.json())
@@ -12,20 +16,20 @@ const App = () => {
       setQuotes(data[randomQuote]);
   });
   };
-
+  //This is my useEffect hook that refreshes the quote
   useEffect(() => {
     getQuote();
   }, []);
 
+  //Below is everything displayed in my app
   return (
     <div className="App">
-    
+      
       <div className="quote">
       <p>Quote: {quotes.text}</p>
       <p>-{quotes.author}</p>
       <button className="btn">Get Quote</button>
-      <a href=""></a>
-      
+      <a href="" className="btn">Tweet</a>
       </div>
       
     </div>
