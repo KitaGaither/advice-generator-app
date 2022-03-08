e3d3c4d2420c4ec1d511b968365d855910b9a09e
Lakita Gaither <lakitagaither@yahoo.com>
2022-03-07 21:43:18 -0600

    Added Quote, Author and Button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import React, { useState, useEffect } from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+  const[quotes, setQuotes] = useState("");
+
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+    .then((res) => res.json())
+    .then((data) => {
+      let randomQuote = Math.floor(Math.random() * data.length);
+      setQuotes(data[randomQuote]);
+  });
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        Advice Generator App(Practice)
-      </header>
+    
+      <div className="quote">
+      <p>Quote: {quotes.text}</p>
+      <p>-{quotes.author}</p>
+      <button className="btn">Get Quote</button>
+      <a href=""></a>
+      
+      </div>
+      
     </div>
   );
-}
-
+  };
 export default App;
